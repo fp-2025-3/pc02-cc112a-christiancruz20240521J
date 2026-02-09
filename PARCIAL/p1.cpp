62bd249b62c02a40bddff9f20b6b897100911d1f
Christian Cruz <christiancruz8326@gmail.com>
2026-02-08 21:32:47 -0500

    termino de resolver p1 del parcial

--- PARCIAL/p1.cpp
+++ PARCIAL/p1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void LlenarArreglo(int *arr, int n){
     for(int i = 0; i < n; i++){
-        *(arr + i) = rand() % 100;
+        *(arr + i) = rand() % 10;
     }
 }
 
@@ -14,6 +14,25 @@ void MostrarArreglo(int *arr, int n){
         cout << *(arr + i) << " ";
     }
     cout << endl;
+}
+
+int MaxCosto(int *A, int n, int i){
+    if(i == n){
+        return 0;
+    }
+
+    int mejor = 0;
+    int producto = 1;
+
+    for(int k = i; k < n; k++){
+        producto *= *(A + k);
+        int costo = producto + MaxCosto(A, n, k + 1);
+        if(costo > mejor){
+            mejor = costo;
+        }
+    }
+
+    return mejor;
 }
 
 int main(){
@@ -30,6 +49,9 @@ int main(){
     cout << "A: ";
     MostrarArreglo(arr, n);
 
+    int rpta = MaxCosto(arr, n, 0);
+
+    cout << "Costo Maximo: " << rpta << endl;
 
     return 0;
 }
